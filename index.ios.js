2f426133f4296d1329471932d5c57d608cc9a98e
Filatov Evgen <evgen.filatov@gmail.com>
2015-05-01 23:33:12 +0300

    created table with sql

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,45 @@ var {
   	Text,
   	View
 } = React;
+function dbConnect() {
+	if (dbConnect.promise) {
+		return dbConnect.promise;
+	}
+	dbConnect.promise = new Promise(function (resolve, reject) {
+		sqlite.open('app.sqlite', function (err, db) {
+			if (err) {
+				return reject(err);
+			}
+			resolve(db);
+		});
+	});
+	return dbConnect.promise;
+}
+function dbCall(query, params) {
+	return dbConnect().then(function (db) {
+		return new Promise(function (resolve, reject) {
+			var rows = [];
+			db.executeSQL(
+				query,
+				params || [],
+				rows.push.bind(rows),
+				function (err) {
+					return err ? reject(err) : resolve(rows);
+				}
+			);
+		});
+	});
+}
+
+dbCall('select name from sqlite_master where type="table" and name="books"').then(function (data) {
+	return data.length || dbCall('create table books(id auto_increment, name varchar(30))');
+}).then(function () {
+	return dbCall('select * from books');
+}).then(function (data) {
+	console.log(data);
+}).catch(function (err) {
+	console.log(err);
+});
 
 var AwesomeProject = React.createClass({
   	render: function() {
