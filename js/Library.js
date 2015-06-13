dc73d8b9fd9e57c6b9b9b6f215c67eb416441042
Filatov Evgen <evgen.filatov@gmail.com>
2015-06-13 19:28:26 +0300

    cleanup

--- js/Library.js
+++ js/Library.js
@@ -20,21 +20,7 @@ function readBooksDir(cb) {
 			});
 		},
 		data => {
-			if (data.length) {
-				return cb(null, data);
-			}
-			fetch('https://downloader.disk.yandex.ru/disk/7c078d4793daa1ac3b888947a385c6aaedd737951009eb229d8c2ef2976afa36/5574633b/3PUrd4R4KxAFOPNm6iQvaoJD0Mh9I7zjMUtu1Bb5Lzq5IZa2T-RFV-3ZHhSI2Xe6BSim3CHJoyRaG6sgkH3qyQ%3D%3D?uid=0&filename=foundation.txt&disposition=attachment&hash=0CPiM4wwBp//hLi/tYHLlE%2BZaOh9UZQyXo3cU%2BC0T7g%3D&limit=0&content_type=text%2Fplain&fsize=398623&hid=3dd09a80b9f6d668ba5c2f93c2817468&media_type=document&tknv=v2')
-				.then(function (data) {
-					return data.text();
-				})
-				.then(function (text) {
-					FileUtil.writeFile('books/text.txt', text, function (err) {
-						if (err) {
-							return cb(err);
-						}
-						return cb(null, ['text.txt']);
-					});
-				});
+			cb(null, data);
 		}
 	);
 }
